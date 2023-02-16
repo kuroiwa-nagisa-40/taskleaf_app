deddca017e5e2399b2d40c642aaa4b9a4b66eb12
kuroiwa-nagisa-40 <naaaaagi.s2.9786@gmail.com>
2023-02-16 23:17:25 +0900

    taskleaf_app

---
+++ db/migrate/20230216134802_adddescription_totasks.rb
@@ -0,0 +1,5 @@
+class AdddescriptionTotasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :description, :text
+  end
+end
---
+++ db/migrate/20230216140918_adddescription_to_tasks.rb
@@ -0,0 +1,5 @@
+class AdddescriptionToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :description, :text
+  end
+end
